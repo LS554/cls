ae00bfacddaa01f7918da8672c6e84a04353e3fb
LS554 <103077889+LS554@users.noreply.github.com>
2025-07-20 19:35:11 -0400

    Update

--- d/cls.d
+++ d/cls.d
@@ -1,3 +1,25 @@
 // Copyright (c) 2025, London Sheard
 // Licensed under the 3-Clause Sheard License.
 // See the LICENSE file in project root for details.
+
+module cls;
+
+import std.stdio;
+import std.exception;
+
+void clear_screen() {
+    
+    try {
+        stdout.write("\033[2J\033[H");
+        stdout.flush();
+    } catch (Exception e) {
+        writef("%s", e.msg);
+    }
+
+    
+}
+
+int main() {
+    clear_screen();
+    return 0;
+}
